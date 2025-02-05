8e5b6af63dccef39484afcd7e7012de58dfdcc0f
Arismonx <arismonx@gmail.com>
2025-02-06 00:47:39 +0700

    [ADD] install godotenv and connect database postgres

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
 
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Read database configuration from .env file
+	host := os.Getenv("DB_HOST")
+	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// Connect to the database using GORM
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	fmt.Println("Database connected", db)
+
+	// Create a new Fiber instance
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
