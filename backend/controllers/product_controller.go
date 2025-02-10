14a81bf9800eec7a71bcd2af485c363db9c43a18
Arismonx <arismonx@gmail.com>
2025-02-10 12:17:30 +0700

    [FIX] add hendle error product_controller

--- backend/controllers/product_controller.go
+++ backend/controllers/product_controller.go
@@ -14,34 +14,56 @@ func CreateProduct(c *fiber.Ctx) error {
 			"error": "Cannot parse JSON",
 		})
 	}
-	config.DB.Create(newProduct)
-	return c.JSON(newProduct)
+
+	if err := config.DB.Create(newProduct).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Cannot Create Product",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(newProduct)
 }
 
 func GetProducts(c *fiber.Ctx) error {
 	var product []models.Products
-	config.DB.Find(&product)
+	if err := config.DB.Find(&product).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Cannot get Products",
+		})
+	}
 	return c.JSON(product)
 }
 
 func GetProductByID(c *fiber.Ctx) error {
 	product := new(models.Products)
 	id := c.Params("id")
-	config.DB.First(&product, id)
+	if err := config.DB.First(&product, id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Product not found",
+		})
+	}
 	return c.JSON(product)
 }
 
 func UpdateProductByID(c *fiber.Ctx) error {
 	product := new(models.Products)
 	id := c.Params("id")
-	config.DB.First(&product, id)
+	if err := config.DB.First(&product, id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Product not found",
+		})
+	}
 
 	if err := c.BodyParser(product); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Cannot parse JSON",
 		})
 	}
-	config.DB.Save(&product)
+	if err := config.DB.Save(&product).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Cannot update product",
+		})
+	}
 
 	return c.JSON(product)
 }
@@ -49,7 +71,18 @@ func UpdateProductByID(c *fiber.Ctx) error {
 func DeleteProductByID(c *fiber.Ctx) error {
 	product := new(models.Products)
 	id := c.Params("id")
-	config.DB.Delete(&product, id)
+
+	if err := config.DB.First(&product, id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Product not found",
+		})
+	}
+
+	if err := config.DB.Delete(&product, id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Cannot delete product",
+		})
+	}
 
 	return c.JSON(fiber.Map{
 		"message": "Delete Successful!",
