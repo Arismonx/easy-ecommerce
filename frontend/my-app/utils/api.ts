ff1b35313da4c13a8e913ead2da37f53f92df646
Arismonx <arismonx@gmail.com>
2025-02-19 20:23:09 +0700

    [Add] add file Dockerfile run backend

--- frontend/my-app/utils/api.ts
+++ frontend/my-app/utils/api.ts
@@ -1,5 +1,5 @@
 export async function login(email: string, password: string) {
-    const res = await fetch("http://localhost:5000/login", {
+    const res = await fetch("http://localhost:8080/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
